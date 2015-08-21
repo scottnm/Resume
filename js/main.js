b559b579bfe8fb352a75ea7dca058b33c0c84f0e
Scott Munro <scottmunro@utexas.edu>
2015-08-21 00:51:55 +0000

    updated so that absolute content lanes top style attr is set based on headerbars dynamic height

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var headerBar = document.querySelector('#header-bar');
+var contentLane = document.querySelector('#content-lane');
+
+
+function prepPage() {
+    contentLane.style.top = headerBar.clientHeight + 'px';
+}
+
+prepPage();
